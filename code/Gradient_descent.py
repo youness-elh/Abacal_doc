added reg and scaling,corrected gradient scaling, refractoring...

code/Gradient_descent.py
@@ -87,22 +87,34 @@ report_file= os.path.join(directory,profiles_file)#exporting data to a created t
 #values of parameters for step 1&2
 Q_step1 = cfg.Gradient_algo['Q_0']
 Q_step2 = cfg.Gradient_algo['Q_1']
+assert len(Q_step1)==len(Q_step1), 'The inputs should be of the same shape!'
+assert len(Q_step1)<6, 'The inputs shape should contains 5 value at maximum!'
 
 nb_col_profiles =cfg.post_Abaqus["extracted_columns"]+3#number of evaluated degrees+3
 #path to the target profiles
 targets_file = cfg.directories['target_file']
 targets_file = os.path.join(directory,targets_file)
 
+###########################################################
+##################adapt to dimension#######################
+###########################################################
+dimension = len(Q_step1) 
+maximum_par = cfg.Gradient_algo['Parameters_interval']['max'][:dimension]
+minimum_par = cfg.Gradient_algo['Parameters_interval']['min'][:dimension]
+
 #########################################################################
 #########################Gradient parameters#############################
 #########################################################################
 
-######################################
+################################################################
 tol = cfg.Gradient_algo['tolernce']
 max_iter = cfg.Gradient_algo['max_iter']
 max_iter_step = cfg.Gradient_algo['max_iter_step']
 step = cfg.Gradient_algo['step']
-#####################################
+gradient_scaling = cfg.Gradient_algo['gradient_scaling'][:dimension]
+regulization = cfg.Gradient_algo['regulization'][:dimension]
+
+################################################################
 
 ###########################################################################
 ###########################################################################
@@ -134,8 +146,8 @@ def profile_target_L2(profile,target):
     output = 1*np.sum(output)#rectangle rule (width = 1 degree)
     return output
 
-def Loss(Q,profile,target,penalisation1=1e-30):#penalisation can be also an array for multi parameters regulation
-    J = profile_target_L2(profile,target) + sum(penalisation1*Q**2) # sum -->in case Q is an array (multi parameters)
+def Loss(Q,profile,target,penalisation=regulization):#penalisation can be also an array for multi parameters regulation
+    J = profile_target_L2(profile,target) + sum(penalisation*Q**2) # sum -->in case Q is an array (multi parameters)
     return J
 
 def unpackarraytostring(Q,space=' '):
@@ -189,7 +201,7 @@ def get_profiles(Q):
 	profiles = Extract(Q)
 	return profiles
 	
-def Loss_calc(Q1,target,penalisation=1e-30):
+def Loss_calc(Q1,target,penalisation=regulization):
 	print('\n--------------------------------------------------------------------')
 	print('----------------------Calculation of Loss--------------------------')
 	print('--------------------------------------------------------------------\n')
@@ -202,7 +214,7 @@ def Loss_calc(Q1,target,penalisation=1e-30):
 #print('the initial loss is=',loss_0)
 
 	
-def Gradient_Loss(L1,Q1,Q2,target,penalisation=1e-30):
+def Gradient_Loss(L1,Q1,Q2,target,penalisation=regulization):
 	#calculate profiles using abqus cmd
 	#assert (Q1 != Q2).any(), "The initial parameter set should be not the same!"
 	assert len(Q1) == len(Q2), "The initial values of parameter array for both steps should be the same!"
@@ -229,8 +241,9 @@ def Gradient_Loss(L1,Q1,Q2,target,penalisation=1e-30):
 			grad[i] = loss[i+1]-loss[0]
 			grad[i] /= Q2[i]-Q1[i]
 	
-	grad[0]=grad[0]*800
-	grad[1:-1]=grad[1:-1]/5000
+	grad[0]=grad[0]*gradient_scaling[0]
+	if n>1:
+		grad[1:]=grad[1:]/gradient_scaling[1:]
 	print('The gradient is ',grad)
 	print('The loss is ',loss)
 
@@ -282,7 +295,7 @@ def save_results(Q_list,Loss_list,Profiles_list,step_list,gradient_list,delta_li
 ################ Calculate initial gradient and loss  ################
 ######################################################################
 def grad_init():
-	gradient_init = Gradient_Loss(Q_step1,Q_step2,Targets,1e-30)
+	gradient_init = Gradient_Loss(Q_step1,Q_step2,Targets,regulization)
 	print("\n")
 	print('Initial gradient---------------------------------------> '+str(gradient_init))
 	return gradient_init
@@ -301,89 +314,89 @@ max_iter_step = max_iter_step
 step = step
 #####################################
 
-######################################################################
-#######################Initialization#################################
-######################################################################
-error = 1.
-error_abs = 1
-iter = 0
-total_iter = 0
-Q_list = [Q_step1,Q_step2]
-L1,profile1 = Loss_calc(Q_list[-2],Targets,1e-30)
-L2,profile2 = Loss_calc(Q_list[-1],Targets,1e-30)
-Loss_list = [L1,L2]
-Profiles_list = [profile1,profile2]
-gradient_list = []
-delta_list = []
-error_list = []
-error_abs_list = []
-step_list = []
-###########################################################
-##################adapt to dimension#######################
-###########################################################
-dimension = len(Q_step1) 
-maximum_par = cfg.Gradient_algo['Parameters_interval']['max'][:dimension]
-minimum_par = cfg.Gradient_algo['Parameters_interval']['min'][:dimension]
-while ((error_abs > tol) and (iter < max_iter)):
+def Gradient_descent(tol,max_iter,max_iter_step,step):
+	######################################################################
+	#######################Initialization#################################
+	######################################################################
+	Q_list = [Q_step1,Q_step2]
+	L1,profile1 = Loss_calc(Q_list[-2],Targets,regulization)
+	L2,profile2 = Loss_calc(Q_list[-1],Targets,regulization)
+	Loss_list = [L1,L2]
+	Profiles_list = [profile1,profile2]
+	gradient_list = []
+	delta_list = []
+	error_list = []
+	error_abs_list = []
+	step_list = []
+	######################
+	error = 1.			##
+	error_abs = 1		##
+	iter = 0			##
+	total_iter = 0		##
+	######################
+	while ((error_abs > tol) and (iter < max_iter)):
 		
-	print('-----------------------------------------------------')
-	print('-----------Optimal Q in iteration '+str(iter)+' = '+str(Q_list[-1])+'-----------')
-	print('-----------------------------------------------------\n')
-	gradient =  Gradient_Loss(Loss_list[-2],Q_list[-2], Q_list[-1],Targets,1e-30)
-	print('----------------------',gradient)
-	Q = np.minimum(np.maximum(np.array(Q_list[-1]) - step*gradient,minimum_par),maximum_par)
-	print('-------gradient and step------',gradient,step)
-	L_new,profiles = Loss_calc(Q,Targets,1e-30)
-	
-	#step of descent direction
-	count = 0
-	Q_old = Q_list[-1]
-	L_old = Loss_list[-1]
-	delta = L_new - L_old 
-	while ((count < max_iter_step)  and (delta >=0)):
-		print('----------------------------------------------------------')
-		print('-----Looking for descent direction for iteration '+str(count+1)+'------')
-		print('----------------------------------------------------------\n')
+		print('-----------------------------------------------------')
+		print('-----------Optimal Q in iteration '+str(iter)+' = '+str(Q_list[-1])+'-----------')
+		print('-----------------------------------------------------\n')
+		gradient =  Gradient_Loss(Loss_list[-2],Q_list[-2], Q_list[-1],Targets,regulization)
+		print('----------------------',gradient)
+		Q = np.minimum(np.maximum(np.array(Q_list[-1]) - step*gradient,minimum_par),maximum_par)
+		print('-------gradient and step------',gradient,step)
+		L_new,profiles = Loss_calc(Q,Targets,regulization)
 
-		step /= 1.3
-		Q = np.minimum(np.maximum(np.array(Q_old) - step*gradient,minimum_par),maximum_par)
-		L_new,profiles = Loss_calc(Q,Targets,1e-30)
-		delta = L_new - L_old
-		count +=1
-		
+		#step of descent direction
+		count = 0
+		Q_old = Q_list[-1]
+		L_old = Loss_list[-1]
+		delta = L_new - L_old 
+		#################################################################
+		##################Backtracking line search#######################
+		#################################################################
+		while ((count < max_iter_step)  and (delta >=0)):
+			print('----------------------------------------------------------')
+			print('-----Looking for descent direction for iteration '+str(count+1)+'------')
+			print('----------------------------------------------------------\n')
+
+			step /= 1.3
+			Q = np.minimum(np.maximum(np.array(Q_old) - step*gradient,minimum_par),maximum_par)
+			L_new,profiles = Loss_calc(Q,Targets,regulization)
+			delta = L_new - L_old
+			count +=1
+			
+			#save
+			delta_list.append(delta)
+			step_list.append(step)
+			Profiles_list.append(profiles)
+			error_abs_list.append(error_abs)
+			
+			#save
+			save_results(Q_list,Loss_list,Profiles_list,step_list,gradient_list,delta_list,error_list,error_abs_list,False)
+
+		total_iter += count + iter
 		#save
-		delta_list.append(delta)
-		step_list.append(step)
-		Profiles_list.append(profiles)
+		if count == 0:
+			Q_list.append(Q)
+			Loss_list.append(L_new)
+			Profiles_list.append(profiles)
+			delta_list.append(delta)
+			step_list.append(step)
+		else:
+			Q_list.append(Q)
+			Loss_list.append(L_new)
+			
+			
+		gradient_list.append(gradient)
+
+		error = abs(Loss_list[-1]-Loss_list[-2])/Loss_list[-2] if Loss_list[-2] != 0 else 0
+		error_list.append(error)
+
+		error_abs = profile_target_L2(profiles,Targets)
 		error_abs_list.append(error_abs)
-		
+		iter += 1
+		print("For iteration "+str(iter)+" we obtain Q = "+str( Q)+ " with a relative error of "+str(error))
 		#save
-		save_results(Q_list,Loss_list,Profiles_list,step_list,gradient_list,delta_list,error_list,error_abs_list,False)
-
-	total_iter += count + iter
-	#save
-	if count == 0:
-		Q_list.append(Q)
-		Loss_list.append(L_new)
-		Profiles_list.append(profiles)
-		delta_list.append(delta)
-		step_list.append(step)
-	else:
-		Q_list.append(Q)
-		Loss_list.append(L_new)
-		
-		
-	gradient_list.append(gradient)
-
-	error = abs(Loss_list[-1]-Loss_list[-2])/Loss_list[-2] if Loss_list[-2] != 0 else 0
-	error_list.append(error)
-	
-	error_abs = profile_target_L2(profiles,Targets)
-	error_abs_list.append(error_abs)
-	iter += 1
-	print("For iteration "+str(iter)+" we obtain Q = "+str( Q)+ " with a relative error of "+str(error))
-	#save
-	save_results(Q_list,Loss_list,Profiles_list,step_list,gradient_list,delta_list,error_list,error_abs_list,True)
+		save_results(Q_list,Loss_list,Profiles_list,step_list,gradient_list,delta_list,error_list,error_abs_list,True)
 
 print('--------------------------------------------------------------------')
 print('----------------------------Done!-----------------------------------')
@@ -393,7 +406,7 @@ print('--------------------------------------------------------------------')
 end_time = time.time()
 elapsed_time = end_time - start_time
 print("Elapsed time to converge "+str(elapsed_time)+" seconds in "+str(total_iter)+" total iterations.\
-\n The search for descent direction took "+str(count)+" in " +str(iter)+" main iterations ")
+\n The search for descent direction took " +str(iter)+" main iterations ")
 with open(state_file,'a') as file:
 	file.write('\n-----------------------------------------------------------------------------------------------\n')
 	file.write("Elapsed time to converge "+str(elapsed_time)+" seconds in "+str(total_iter)+" total iterations including search for descent direction and " +str(iter)+" main iterations ")

code/config.py
@@ -50,8 +50,10 @@ Gradient_algo = {
 				},
 				'tolernce': 1.e-1,
 				'max_iter':10,
-				'max_iter_step':24,
+				'max_iter_step':10,
 				'step':1.,
+				'regulization':np.array([1e-30,1e-30,1e-30,1e-30,1e-30]),
+				'gradient_scaling':np.array([800,5000,5000,5000,5000]),
 }
 
 
@@ -60,7 +62,7 @@ output=  {
 				'state_file':'state.txt',
 				'Results':{
 						'Loss_list':'results/Loss_list.txt',
-						'Profiles_list':'results/Loss_list.txt',
+						'Profiles_list':'results/Profiles_list.txt',
 						'Q_list':'results/Q_list.txt',
 						'delta_list':'results/delta_list.txt',
 						'step_list':'results/step_list.txt',